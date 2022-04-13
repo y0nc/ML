a4f82a6bcb7881fe88a795027701016e23c449c6
sinceredipity <1766670160@qq.com>
2022-04-13 13:36:30 +0800

    4/13/2022

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -12,10 +12,10 @@ class NaiveBayes:
 
         is_discrate, cnt, = None, None
 
-    data = []  # 统计信息
-    Y = []  # 标签集
-    tot, ycnt = 0, {}  # 统计信息
-    Ni = []  # 统计信息
+    data = []
+    Y = []
+    tot, ycnt = 0, {}
+    Ni = []
 
     def __init__(self, D) -> None:
         self.tot = len(D)
@@ -80,7 +80,7 @@ df = pd.read_csv("watermelon3_0_Ch.csv")
 df.drop("编号", axis=1, inplace=True)
 t0 = time()
 nb = NaiveBayes(df)
-print("训练消耗时间: {:.3f}S".format(time() - t0))
+print("统计消耗时间: {:.3f}S".format(time() - t0))
 
 precision = 0.0
 
@@ -88,7 +88,7 @@ t0 = time()
 for i in range(len(df)):
     x = df.iloc[i, :].tolist()
     y = nb.predict(x[:-1])
-    print("{}: {}\n真实分类: {}, 预测结果: {}".format(i + 1, x, x[-1], y))
+    print("{}: 真实分类: {}, 预测结果: {}".format(i + 1, x[-1], y))
     if x[-1] == y:
         precision += 1 / (len(df) + 1)
 
