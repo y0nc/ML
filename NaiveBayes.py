036e2116af0a6569664e3e6d4d1f1bb74f7cf478
sinceredipity <1766670160@qq.com>
2022-04-13 14:27:48 +0800

    4/13/2022

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,7 +1,9 @@
 from numpy import float64
+import numpy as np
 import pandas as pd
 from time import time
 from scipy import stats as st
+from sklearn import datasets
 
 
 class NaiveBayes:
@@ -12,12 +14,21 @@ class NaiveBayes:
 
         is_discrate, cnt, = None, None
 
-    data = []
-    Y = []
-    tot, ycnt = 0, {}
-    Ni = []
+    data = None
+    Y = None
+    tot = None
+    ycnt = None
+    Ni = None
+
+    def malloc(self):
+        self.data = []
+        self.Y = []
+        self.tot = 0
+        self.ycnt = {}
+        self.Ni = []
 
     def __init__(self, D) -> None:
+        self.malloc()
         self.tot = len(D)
         self.Y = set(D.iloc[:, -1])
         Dy = {}
@@ -72,25 +83,4 @@ class NaiveBayes:
 
             if p > maxp:
                 maxp, res = p, y
-
         return res
-
-
-df = pd.read_csv("watermelon3_0_Ch.csv")
-df.drop("编号", axis=1, inplace=True)
-t0 = time()
-nb = NaiveBayes(df)
-print("统计消耗时间: {:.3f}S".format(time() - t0))
-
-precision = 0.0
-
-t0 = time()
-for i in range(len(df)):
-    x = df.iloc[i, :].tolist()
-    y = nb.predict(x[:-1])
-    print("{}: 真实分类: {}, 预测结果: {}".format(i + 1, x[-1], y))
-    if x[-1] == y:
-        precision += 1 / (len(df) + 1)
-
-print("准确率: {:.3f}".format(precision))
-print("预测消耗时间: {:.3f}S".format(time() - t0))
---
+++ main.py
@@ -0,0 +1,46 @@
+from NaiveBayes import NaiveBayes
+import numpy as np
+from sklearn import datasets
+from time import time
+import pandas as pd
+
+
+df = pd.read_csv("watermelon3_0_Ch.csv")
+df.drop("编号", axis=1, inplace=True)
+t0 = time()
+nb = NaiveBayes(df)
+print("统计消耗时间: {:.3f}S".format(time() - t0))
+
+precision = 0.0
+
+t0 = time()
+for i in range(len(df)):
+    x = df.iloc[i, :].tolist()
+    y = nb.predict(x[:-1])
+    print("{}: 真实分类: {}, 预测结果: {}".format(i + 1, x[-1], y))
+    if x[-1] == y:
+        precision += 1 / (len(df) + 1)
+
+print("准确率: {:.3f}".format(precision))
+print("预测消耗时间: {:.3f}S".format(time() - t0))
+
+
+iris = datasets.load_iris()
+df = pd.DataFrame(
+    data=np.c_[iris["data"], iris["target"]], columns=iris["feature_names"] + ["target"]
+)
+print(df)
+t0 = time()
+nb = NaiveBayes(df)
+print("统计消耗时间: {:.3f}S".format(time() - t0))
+precision = 0.0
+t0 = time()
+for i in range(len(df)):
+    x = df.iloc[i, :].tolist()
+    y = nb.predict(x[:-1])
+    print("{}: 真实分类: {}, 预测结果: {}".format(i + 1, x[-1], y))
+    if x[-1] == y:
+        precision += 1 / (len(df) + 1)
+
+print("准确率: {:.3f}".format(precision))
+print("预测消耗时间: {:.3f}S".format(time() - t0))
